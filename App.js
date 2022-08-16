8ebb5cfb7b7200e48268e9e957bf7d28d7f292f2
Ioan Zaharia <zaharia.ioan@gmail.com>
2022-08-16 13:48:40 +0200

    basic layout, orientation, flex and position

--- App.js
+++ App.js
@@ -1,11 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Dimensions,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  Text,
+  View,
+} from 'react-native';
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from '@react-native-community/hooks';
 
 export default function App() {
+  // console.log(Dimensions.get('window'));
+  console.log(useDimensions());
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <SafeAreaView style={styles.container}>
+    //   <View
+    //     style={{
+    //       backgroundColor: 'dodgerblue',
+    //       width: '100%',
+    //       height: landscape ? '100%' : '30%',
+    //     }}
+    //   ></View>
+    // </SafeAreaView>
+    <View
+      style={{
+        backgroundColor: '#fff',
+        flex: 1,
+        flexDirection: 'row', //default in react native is "column"
+        justifyContent: 'center',
+        alignItems: 'center',
+        // alignContent: 'center', //works only if wrapping is enabled
+        // flexWrap: 'wrap',
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          // flex: 2,
+          // flexBasis: 200, // width/height related to the main axes (row/column)
+          // flexGrow: 1, // takes all available space
+          // flexShrink: 1, //shrink so other items can fit
+          width: 100,
+          height: 100,
+          // alignSelf: 'flex-end',
+        }}
+      />
+
+      <View
+        style={{
+          backgroundColor: 'gold',
+          // flex: 1,
+          width: 100,
+          height: 100,
+          // position: 'relative', //by default
+          top: -20,
+        }}
+      />
+
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          // flex: 1,
+          width: 100,
+          height: 100,
+          right: 30,
+        }}
+      />
+      {/* <View
+        style={{
+          backgroundColor: 'grey',
+          // flex: 1,
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'greenyellow',
+          // flex: 1,
+          width: 100,
+          height: 100,
+        }}
+      /> */}
     </View>
   );
 }
@@ -14,7 +95,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
